d340f28ee0bc97481cbff15e64eb67192d68b315
Dmytro Romas <d.romas@mobidev.biz>
2023-09-04 11:12:00 +0300

    Homework - Lesson 1

---
+++ Master/Lesson1/script.js
@@ -0,0 +1,10 @@
+let userText = prompt("Please, enter your name");
+console.log(userText);
+alert(
+  ' "Hello,' +
+    " " +
+    userText +
+    "!" +
+    " " +
+    'How are you? This is JavaScript world!" '
+);
